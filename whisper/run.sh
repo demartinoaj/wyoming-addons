7067e9b2af15e22b9b006e43217bd89d424114f1
Andrew DeMartino <demartinoaj@gmail.com>
2024-05-12 23:37:10 -0700

    Adding NVIDA Cuda GPU support to faster-whisper

--- whisper/run.sh
+++ whisper/run.sh
@@ -2,4 +2,6 @@
 python3 -m wyoming_faster_whisper \
     --uri 'tcp://0.0.0.0:10300' \
     --data-dir /data \
-    --download-dir /data "$@"
+    --download-dir /data "$@" \
+    --device cuda
+# add device == cuda for GPU support
